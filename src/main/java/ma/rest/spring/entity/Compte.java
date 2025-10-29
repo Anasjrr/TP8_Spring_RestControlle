7b3263fc06c7b302c239e21517ff4116fe3b1933
Anasjrr <anasKacm89@gmail.com>
2025-10-29 23:40:17 +0100

    tp8

---
+++ src/main/java/ma/rest/spring/entity/Compte.java
@@ -0,0 +1,28 @@
+package ma.rest.spring.entity;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double solde;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+
+    @Enumerated(EnumType.STRING)
+    private TypeCompte type;
+}
---
+++ src/main/java/ma/rest/spring/repository/CompteRepository.java
@@ -0,0 +1,9 @@
+package ma.rest.spring.repository;
+
+import ma.rest.spring.entity.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
